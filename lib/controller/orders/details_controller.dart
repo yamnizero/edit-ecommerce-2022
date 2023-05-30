fdc6c8d9786241134cb964a547b0357c2703e63d
mohammed jamal <yamni.zero@gmail.com>
2023-05-31 00:16:38 +0200

    UI OrderDetails Screen 2

--- lib/controller/orders/details_controller.dart
+++ lib/controller/orders/details_controller.dart
@@ -1,13 +1,32 @@
+import 'dart:async';
+
 import 'package:ecommerc_2022/data/model/orders_model.dart';
+
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class OrdersDetailsController extends GetxController{
 
   late OrdersModel ordersModel ;
 
+  Completer<GoogleMapController>? completerController;
+  List<Marker> markers =[];
+  double? lat;
+  double? long;
+
+  CameraPosition? cameraPosition ;
+  initialData(){
+    cameraPosition =  CameraPosition(
+      target: LatLng(35.22, 37.3),
+      zoom: 12.4746,
+    );
+  }
+
   @override
   void onInit() {
+
     ordersModel = Get.arguments['ordersModel'];
+    initialData();
     // TODO: implement onInit
     super.onInit();
   }
--- lib/view/screen/orders/order_details.dart
+++ lib/view/screen/orders/order_details.dart
@@ -3,6 +3,7 @@ import 'package:ecommerc_2022/core/constant/color.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class OrdersDetails extends StatelessWidget {
   const OrdersDetails({Key? key}) : super(key: key);
@@ -15,45 +16,116 @@ class OrdersDetails extends StatelessWidget {
         title: const Text('OrdersDetails'),
       ),
       body: Container(
-        padding: const EdgeInsets.symmetric(horizontal: 10),
+        padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
         child: ListView(
           children: [
-            Table(
-              children: const [
-                TableRow(
-                  children: [
-                    Text("Item",textAlign: TextAlign.center,style: TextStyle(color: AppColor.primaryColor,fontWeight: FontWeight.bold),),
-                    Text("QTY",textAlign: TextAlign.center,style: TextStyle(color: AppColor.primaryColor,fontWeight: FontWeight.bold),),
-                    Text("Price",textAlign: TextAlign.center,style: TextStyle(color: AppColor.primaryColor,fontWeight: FontWeight.bold),),
-                  ]
-                ),
-                TableRow(
-                  children: [
-                    Text("MacBook M1",textAlign: TextAlign.center,),
-                    Text("2",textAlign: TextAlign.center,),
-                    Text("1200",textAlign: TextAlign.center,),
-                  ]
-                ),
-                TableRow(
-                  children: [
-                    Text("IPhone 14",textAlign: TextAlign.center,),
-                    Text("2",textAlign: TextAlign.center,),
-                    Text("4000",textAlign: TextAlign.center,),
-                  ]
-                ),
-              ],
-            ),
-            const SizedBox(height: 10,),
-            Container(
-                padding: const EdgeInsets.symmetric(horizontal: 10),
-                child: const Text("Price : 2700\$" , textAlign: TextAlign.center,style: TextStyle(color: AppColor.primaryColor,fontWeight: FontWeight.bold))),
-            const SizedBox(height: 10,),
             Card(
+              child: Padding(
+                padding: const EdgeInsets.symmetric(vertical: 20),
+                child: Table(
+                  children: const [
+                    TableRow(children: [
+                      Text(
+                        "Item",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: AppColor.primaryColor,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "QTY",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: AppColor.primaryColor,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Text(
+                        "Price",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            color: AppColor.primaryColor,
+                            fontWeight: FontWeight.bold),
+                      ),
+                    ]),
+                    TableRow(children: [
+                      Text(
+                        "MacBook M1",
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        "2",
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        "1200",
+                        textAlign: TextAlign.center,
+                      ),
+                    ]),
+                    TableRow(children: [
+                      Text(
+                        "IPhone 14",
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        "2",
+                        textAlign: TextAlign.center,
+                      ),
+                      Text(
+                        "4000",
+                        textAlign: TextAlign.center,
+                      ),
+                    ]),
+                  ],
+                ),
+              ),
+            ),
+            Card(
+              child: Container(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                child: const Text(
+                  "Price : 2700\$",
+                  // textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 16,
+                      color: AppColor.primaryColor,
+                      fontWeight: FontWeight.bold),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            const Card(
               child: ListTile(
-                title: Text('Address'),
+                title: Text(
+                  'Address',
+                  style: TextStyle(
+                      fontSize: 16,
+                      color: AppColor.primaryColor,
+                      fontWeight: FontWeight.bold),
+                ),
                 subtitle: Text('AlDisa streeat '),
               ),
-            )
+            ),
+            Card(
+              child: Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10 ,vertical: 10),
+                height: 300,
+                width: double.infinity,
+                child:   GoogleMap(
+                  mapType: MapType.normal,
+                  markers: controller.markers.toSet(),
+                  initialCameraPosition:
+                  controller.cameraPosition!,
+                  onMapCreated:
+                      (GoogleMapController controllerMap) {
+                        controller.completerController!
+                        .complete(controllerMap);
+                  },
+                ),
+              ),
+            ),
           ],
         ),
       ),
